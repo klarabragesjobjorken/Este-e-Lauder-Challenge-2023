0494fea3cd06f18cb22059b72734e8dec8512ad6
Klara Bragesjö Björkén <klara.bjorken@gmail.com>
2023-09-02 00:13:52 +0200

    Initial Commit

---
+++ Estée Lauder Challenge 2023/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Estée Lauder Challenge 2023
+//
+//  Created by Klara Björkén on 2023-09-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Estée Lauder Challenge 2023/Este_e_Lauder_Challenge_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Este_e_Lauder_Challenge_2023App.swift
+//  Estée Lauder Challenge 2023
+//
+//  Created by Klara Björkén on 2023-09-02.
+//
+
+import SwiftUI
+
+@main
+struct Este_e_Lauder_Challenge_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
